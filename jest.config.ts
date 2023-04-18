bbbe65123322548aa3ad1e98e66049cfbdff0ca2
clement.tls <clement.tolois@gmail.com>
2023-04-19 01:19:37 +0200

    Feat: Jest can resolve test

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+import { JestConfigWithTsJest } from "ts-jest";
+
+const jestConfig: JestConfigWithTsJest = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+};
+
+export default jestConfig;
--- src/__tests__/Welcome.test.ts
+++ src/__tests__/Welcome.test.ts
@@ -1,5 +1,9 @@
+
+import Welcome from "./../core/Welcome";
+
 describe("Should test the greetings", () => {
     it("Should console.log the greetings", () => {
+        expect(Welcome.greetings()).toBe(undefined);
         expect(true).toBe(true);
     });
 });
