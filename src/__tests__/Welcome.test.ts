60c5826c9875327c3e16f34a472fd7ba6c42943b
clement.tls <clement.tolois@gmail.com>
2023-04-20 10:20:55 +0200

    Chore: Use tabs instead of spaces

--- src/__tests__/Welcome.test.ts
+++ src/__tests__/Welcome.test.ts
@@ -2,8 +2,8 @@
 import Welcome from "@/core/Welcome";
 
 describe("Should test the greetings", () => {
-    it("Should console.log the greetings", () => {
-        expect(Welcome.greetings()).toBe(undefined);
-        expect(true).toBe(true);
-    });
+	it("Should console.log the greetings", () => {
+		expect(Welcome.greetings()).toBe(undefined);
+		expect(true).toBe(true);
+	});
 });
