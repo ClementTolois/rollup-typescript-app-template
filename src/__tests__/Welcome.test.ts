a019219174e6499ff941d154abac67ae513c340b
clement.t <clement.tolois@gmail.com>
2023-04-21 16:13:06 +0200

    Chore: Use singlequote for eslint

--- src/__tests__/Welcome.test.ts
+++ src/__tests__/Welcome.test.ts
@@ -1,8 +1,8 @@
 
-import Welcome from "@/core/Welcome";
+import Welcome from '@/core/Welcome';
 
-describe("Should test the greetings", () => {
-	it("Should console.log the greetings", () => {
+describe('Should test the greetings', () => {
+	it('Should console.log the greetings', () => {
 		expect(Welcome.greetings()).toBe(undefined);
 		expect(true).toBe(true);
 	});
